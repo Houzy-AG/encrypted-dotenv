088ba7a23162815f09a101b44d77d43923401abe
Nicolae Bacila <nicolae.bacila@houzy.ch>
2024-09-02 09:38:44 +0300

    Make logger optional

--- src/core/configure.ts
+++ src/core/configure.ts
@@ -4,7 +4,7 @@ import { defaultLogger, EncryptedEnvLogger } from './logger/encrypted-env-logger
 // Return the env vars for the current environment by merging the process.env with the local .env file and with the vault variables for the
 // current environment.
 // current environment.
-export const configure = (options?: { dotEnvFilesDirectory?: string; logger: EncryptedEnvLogger }): void => {
+export const configure = (options?: { dotEnvFilesDirectory?: string; logger?: EncryptedEnvLogger }): void => {
     const vaultEnvironmentsManager = createVaultEnvironmentsManager({
         logger: options?.logger ?? defaultLogger,
         dotEnvFilesDirectory: options?.dotEnvFilesDirectory ?? ``,
